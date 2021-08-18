30c410e90600c89d6da89b752fdee14d5965d722
elevenChu <964737042@qq.com>
2021-08-18 11:18:46 +0800

    8.18-1.1

--- src/main/java/com/elevenchu/wiki/service/EbookService.java
+++ src/main/java/com/elevenchu/wiki/service/EbookService.java
@@ -1,12 +1,21 @@
 package com.elevenchu.wiki.service;
 
 import com.elevenchu.wiki.mapper.EbookMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
-    EbookMapper ebookMapper;
+    private EbookMapper ebookMapper;
+
+    
 }
