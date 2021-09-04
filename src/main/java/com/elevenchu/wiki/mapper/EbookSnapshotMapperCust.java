4c64c1c38bb264868e50a29dc4f5f25c33c85205
elevenChu <964737042@qq.com>
2021-09-04 16:08:08 +0800

    9.4-统计数值功能的开发

--- src/main/java/com/elevenchu/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/elevenchu/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,8 +1,12 @@
 package com.elevenchu.wiki.mapper;
 
+import com.elevenchu.wiki.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
     public void genSnapshot();
-//    List<StatisticResp> getStatistic();
+    List<StatisticResp> getStatistic();
 //
-//    List<StatisticResp> get30Statistic();
+    List<StatisticResp> get30Statistic();
 }
