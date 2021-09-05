faae121e986487228d373d1044f8d46ecb06bb05
elevenChu <964737042@qq.com>
2021-09-05 22:45:45 +0800

    9.5-拦截器与程序启动部分

---
+++ src/main/java/com/elevenchu/wiki/util/LoginUserContext.java
@@ -0,0 +1,19 @@
+package com.elevenchu.wiki.util;
+
+import com.elevenchu.wiki.resp.UserLoginResp;
+
+import java.io.Serializable;
+
+public class LoginUserContext implements Serializable {
+
+    private static ThreadLocal<UserLoginResp> user = new ThreadLocal<>();
+
+    public static UserLoginResp getUser() {
+        return user.get();
+    }
+
+    public static void setUser(UserLoginResp user) {
+        LoginUserContext.user.set(user);
+    }
+
+}
