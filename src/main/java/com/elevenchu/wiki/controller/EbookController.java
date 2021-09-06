669fe0d2071d6ebff393693da715032bba4d94b7
elevenChu <964737042@qq.com>
2021-09-06 15:32:27 +0800

    9.6-Doc与Ebook表现层业务层代码修改

--- src/main/java/com/elevenchu/wiki/controller/EbookController.java
+++ src/main/java/com/elevenchu/wiki/controller/EbookController.java
@@ -1,10 +1,42 @@
 package com.elevenchu.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.elevenchu.wiki.req.EbookQueryReq;
+import com.elevenchu.wiki.req.EbookSaveReq;
+import com.elevenchu.wiki.resp.CommonResp;
+import com.elevenchu.wiki.resp.EbookQueryResp;
+import com.elevenchu.wiki.resp.PageResp;
+import com.elevenchu.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/ebook")
 public class EbookController {
 
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid EbookQueryReq req) {
+        CommonResp<PageResp<EbookQueryResp>> resp = new CommonResp<>();
+        PageResp<EbookQueryResp> list = ebookService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody EbookSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        ebookService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        ebookService.delete(id);
+        return resp;
+    }
 }
--- src/main/java/com/elevenchu/wiki/service/EbookService.java
+++ src/main/java/com/elevenchu/wiki/service/EbookService.java
@@ -1,6 +1,16 @@
 package com.elevenchu.wiki.service;
 
+import com.elevenchu.wiki.domain.Ebook;
+import com.elevenchu.wiki.domain.EbookExample;
 import com.elevenchu.wiki.mapper.EbookMapper;
+import com.elevenchu.wiki.req.EbookQueryReq;
+import com.elevenchu.wiki.req.EbookSaveReq;
+import com.elevenchu.wiki.resp.EbookQueryResp;
+import com.elevenchu.wiki.resp.PageResp;
+import com.elevenchu.wiki.util.CopyUtil;
+import com.elevenchu.wiki.util.SnowFlake;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -17,5 +27,65 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    
+    @Resource
+    private SnowFlake snowFlake;
+
+    public PageResp<EbookQueryResp> list(EbookQueryReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        if (!ObjectUtils.isEmpty(req.getName())) {
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
+        if (!ObjectUtils.isEmpty(req.getCategoryId2())) {
+            criteria.andCategory2IdEqualTo(req.getCategoryId2());
+        }
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        // List<EbookResp> respList = new ArrayList<>();
+        // for (Ebook ebook : ebookList) {
+        //     // EbookResp ebookResp = new EbookResp();
+        //     // BeanUtils.copyProperties(ebook, ebookResp);
+        //     // 对象复制
+        //     EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+        //
+        //     respList.add(ebookResp);
+        // }
+
+        // 列表复制
+        List<EbookQueryResp> list = CopyUtil.copyList(ebookList, EbookQueryResp.class);
+
+        PageResp<EbookQueryResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+
+        return pageResp;
+    }
+
+    /**
+ * 保存
+ */
+public void save(EbookSaveReq req) {
+    Ebook ebook = CopyUtil.copy(req, Ebook.class);
+    if (ObjectUtils.isEmpty(req.getId())) {
+        // 新增
+        ebook.setId(snowFlake.nextId());
+        ebookMapper.insert(ebook);
+    } else {
+        // 更新
+        ebookMapper.updateByPrimaryKey(ebook);
+    }
+}
+
+    /**
+     * 删除
+     * @param id
+     */
+    public void delete(Long id) {
+        ebookMapper.deleteByPrimaryKey(id);
+    }
 }
